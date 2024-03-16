d5e776d8bdf35d866e12495e9b780a820f8b9798
Cesmac <cesmac@academico.cesmac.edu.br>
2024-03-16 17:00:41 -0300

    estilizando site djangogirls

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+from .models import Post
+from django.shortcuts import render, get_object_or_404
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 # Create your views here.
